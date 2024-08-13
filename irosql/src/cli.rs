b3067517571bf9f27c0c4dd1df778133175dbdbd
ariel <ariellira.al@gmail.com>
2024-08-13 15:33:28 -0700

    created cli args for irosql

---
+++ irosql/src/cli.rs
@@ -0,0 +1,48 @@
+use clap::{Parser, Subcommand};
+use sql_formatter::settings::{FormatterSettings, IndentCommaLists, KeywordCase};
+
+#[derive(Parser, Debug, Clone)]
+pub struct Format {
+    pub input: String,
+    #[arg(short = 'c', long)]
+    pub indent_comma_lists: Option<IndentCommaLists>,
+    #[arg(short = 'i', long, default_value_t = false)]
+    pub indent_in_lists: bool,
+    #[arg(short = 'b', long, default_value_t = false)]
+    pub indent_between_conditions: bool,
+    #[arg(short, long, default_value_t = KeywordCase::Upper)]
+    pub keyword_case: KeywordCase,
+    #[arg(short, long, default_value_t = 80)]
+    pub max_width: u32,
+    #[arg(short = 'w', long, default_value_t = 4)]
+    pub indent_width: u32,
+    #[arg(short, long, default_value_t = false)]
+    pub use_tab: bool,
+}
+
+#[derive(Subcommand, Debug, Clone)]
+pub enum Command {
+    #[command(name = "format", about = "Formats T-SQL code in opionated style")]
+    Format(Format),
+}
+
+#[derive(Parser, Debug, Clone)]
+#[command(name = "irosql", version = "0.1.0", author = "Ariel")]
+pub struct Cli {
+    #[command(subcommand)]
+    pub command: Command,
+}
+
+impl From<Format> for FormatterSettings {
+    fn from(value: Format) -> Self {
+        Self {
+            indent_comma_lists: value.indent_comma_lists,
+            indent_in_lists: value.indent_in_lists,
+            indent_between_conditions: value.indent_between_conditions,
+            keyword_case: value.keyword_case,
+            max_width: value.max_width,
+            indent_width: value.indent_width,
+            use_tab: value.use_tab,
+        }
+    }
+}
--- irosql/src/main.rs
+++ irosql/src/main.rs
@@ -1,55 +1,22 @@
-use clap::{Parser, Subcommand};
-use sql_formatter::formatter::Formatter;
-use sql_formatter::settings::{FormatterSettings, IndentCommaLists, KeywordCase};
+use clap::Parser as _;
+use cli::Cli;
+use sql_formatter::{formatter::Formatter, settings::FormatterSettings};
 
-#[derive(Parser, Debug)]
-struct Format {
-    input: String,
-    #[arg(short = 'c', long)]
-    indent_comma_lists: Option<IndentCommaLists>,
-    #[arg(short = 'i', long, default_value_t = false)]
-    indent_in_lists: bool,
-    #[arg(short = 'b', long, default_value_t = false)]
-    indent_between_conditions: bool,
-    #[arg(short, long, default_value_t = KeywordCase::Upper)]
-    keyword_case: KeywordCase,
-    #[arg(short, long, default_value_t = 80)]
-    max_width: u32,
-    #[arg(short = 'w', long, default_value_t = 4)]
-    indent_width: u32,
-    #[arg(short, long, default_value_t = false)]
-    use_tab: bool,
-}
-
-#[derive(Subcommand, Debug)]
-enum Command {
-    #[command()]
-    Format(Format)
-}
-
-#[derive(Parser)]
-#[command(name = "irosql", version = "0.1.0", author = "Ariel")]
-struct Cli {
-    #[command(subcommand)]
-    command: Command,
-}
+mod cli;
 
 fn main() {
     let cli = Cli::parse();
-    let formatter_settings = FormatterSettings {
-        indent_comma_lists: cli.indent_comma_lists,
-        indent_in_lists: cli.indent_in_lists,
-        indent_between_conditions: cli.indent_between_conditions,
-        keyword_case: cli.keyword_case,
-        max_width: cli.max_width,
-        indent_width: cli.indent_width,
-        use_tab: cli.use_tab,
-    };
-    let mut formatter = Formatter::new(formatter_settings);
-    if let Err(e) = formatter.format(&cli.input) {
-        eprintln!("Error: {}", e);
-        return;
-    }
+    match cli.command {
+        cli::Command::Format(format_args) => {
+            let input = format_args.input.clone();
+            let formatter_settings: FormatterSettings = format_args.into();
+            let mut formatter = Formatter::new(formatter_settings);
+            if let Err(e) = formatter.format(input.as_str()) {
+                eprintln!("Error: {}", e);
+                return;
+            }
 
-    println!("{}", formatter.formatted_query());
+            println!("{}", formatter.formatted_query());
+        }
+    }
 }
