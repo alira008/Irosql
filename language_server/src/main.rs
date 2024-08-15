a27047f5c0f6ecfc4a41b8f4e2e0df60d6d51340
Ariel Lira <ariellira.al@gmail.com>
2024-08-15 14:11:20 -0700

    started working on a test language_server

--- language_server/src/main.rs
+++ language_server/src/main.rs
@@ -1,3 +1,125 @@
+use dashmap::DashMap;
+use ropey::Rope;
+use tower_lsp::jsonrpc::Result;
+use tower_lsp::lsp_types::*;
+use tower_lsp::{Client, LanguageServer, LspService, Server};
+
+#[derive(Debug)]
+struct Backend {
+    client: Client,
+    ast_map: DashMap<String, parser::ast::Query>,
+    document_map: DashMap<String, Rope>,
+}
+
+#[tower_lsp::async_trait]
+impl LanguageServer for Backend {
+    async fn initialize(&self, _: InitializeParams) -> Result<InitializeResult> {
+        Ok(InitializeResult::default())
+    }
+
+    async fn initialized(&self, _: InitializedParams) {
+        self.client
+            .log_message(MessageType::INFO, "server initialized")
+            .await;
+    }
+
+    async fn shutdown(&self) -> Result<()> {
+        Ok(())
+    }
+
+    async fn did_change(&self, mut params: DidChangeTextDocumentParams) {
+        self.on_change(TextDocumentItem {
+            uri: params.text_document.uri,
+            text: std::mem::take(&mut params.content_changes[0].text),
+            version: params.text_document.version,
+        })
+        .await
+    }
+
+    async fn did_close(&self, _: DidCloseTextDocumentParams) {
+        self.client
+            .log_message(MessageType::INFO, "file closed!")
+            .await;
+    }
+
+    async fn completion(&self, params: CompletionParams) -> Result<Option<CompletionResponse>> {
+        let uri = params.text_document_position.text_document.uri;
+        let position = params.text_document_position.position;
+        let completions = || -> Option<Vec<CompletionItem>> {
+            let rope = self.document_map.get(uri.as_str())?;
+            let ast = self.ast_map.get(uri.as_str())?;
+            let char = rope.try_line_to_char(position.line as usize).ok()?;
+            let offset = char + position.character as usize;
+            None
+        };
+
+        Ok(None)
+    }
+}
+
+struct TextDocumentItem {
+    uri: Url,
+    text: String,
+    version: i32,
+}
+
+impl Backend {
+    async fn on_change(&self, params: TextDocumentItem) {
+        let rope = ropey::Rope::from_str(&params.text);
+        self.document_map
+            .insert(params.uri.to_string(), rope.clone());
+        let lexer = lexer::Lexer::new(&params.text);
+        let mut parser = parser::Parser::new(lexer);
+        let query = parser.parse();
+
+        let diagnostics = parser
+            .errors()
+            .iter()
+            .filter_map(|item| {
+                let message = item.details();
+                let span = item.span;
+
+                || -> Option<Diagnostic> {
+                    let start_position = offset_to_position(span.start as usize, &rope)?;
+                    let end_position = offset_to_position(span.end as usize, &rope)?;
+                    Some(Diagnostic::new_simple(
+                        Range::new(start_position, end_position),
+                        message,
+                    ))
+                }()
+            })
+            .collect::<Vec<_>>();
+
+        self.client
+            .publish_diagnostics(params.uri.clone(), diagnostics, Some(params.version))
+            .await;
+
+        self.ast_map.insert(params.uri.to_string(), query);
+    }
+}
+
+fn offset_to_position(offset: usize, rope: &Rope) -> Option<Position> {
+    let line = rope.try_char_to_line(offset).ok()?;
+    let first_char_of_line = rope.try_line_to_char(line).ok()?;
+    let column = offset - first_char_of_line;
+    Some(Position::new(line as u32, column as u32))
+}
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = tokio::io::stdin();
+    let stdout = tokio::io::stdout();
+
+    let (service, socket) = LspService::new(|client| Backend {
+        client,
+        ast_map: DashMap::default(),
+        document_map: DashMap::default(),
+    });
+    tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .unwrap()
+        .block_on(async {
+            println!("hello world");
+            Server::new(stdin, stdout, socket).serve(service).await;
+        });
 }
